707d38185f8b4c9e5c82236702cdbc35e01b89b9
Bastien Teissier <bastient@theodo.fr>
2026-03-02 22:41:52 +0100

    feat(store): implement product detail page

--- store/src/App.tsx
+++ store/src/App.tsx
@@ -5,6 +5,7 @@ import { CartPage } from "./pages/CartPage.js";
 import { CheckoutPage } from "./pages/CheckoutPage.js";
 import { HomePage } from "./pages/HomePage.js";
 import { OrderConfirmationPage } from "./pages/OrderConfirmationPage.js";
+import { ProductDetailPage } from "./pages/ProductDetailPage.js";
 
 export function App() {
 	return (
@@ -12,6 +13,7 @@ export function App() {
 			<CartIndicator />
 			<Routes>
 				<Route path="/" element={<HomePage />} />
+				<Route path="/products/:id" element={<ProductDetailPage />} />
 				<Route path="/cart" element={<CartPage />} />
 				<Route path="/checkout" element={<CheckoutPage />} />
 				<Route path="/orders/:reference" element={<OrderConfirmationPage />} />
